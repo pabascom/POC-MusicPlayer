d814a809bccde98537446b4bf07c3b65a515cecf
pabascom <phillip.bascom@mobileappscompany.us>
2019-05-13 21:49:07 -0400

    first commit

---
+++ app/src/main/java/phil/homework/week2day3_musicplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package phil.homework.week2day3_musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
